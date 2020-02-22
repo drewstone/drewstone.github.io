b30d2e4b5c0f9388222d5d9cda91addce9179dea
Drew Stone <drewstone329@gmail.com>
2020-02-23 01:13:31 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
+  assetPrefix: isProd ? '/' : '',
 });
